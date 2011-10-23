d9b8d6889b82a06736399dfb56b70b57964774aa
James Booth <boothj5@gmail.com>
2011-10-23 18:55:10 +0100

    More game tests

--- test/unit/test_game.rb
+++ test/unit/test_game.rb
@@ -2,7 +2,7 @@ require './lib/game.rb'
 require 'test/unit'
 
 class TestGame < Test::Unit::TestCase
-    
+   
     def create_player_names
         names = Array.new
         names.push "James"
@@ -100,4 +100,81 @@ class TestGame < Test::Unit::TestCase
         assert pile_size_before < game.pile.size
         assert deck_size_before > game.deck.size
     end 
+
+    def test_first_move_lays_both_if_same
+        names = create_player_names
+        game = Game.new names, 3
+        eleven = Card.new 11, 4
+        three = Card.new 3, 3
+        another_three = Card.new 3, 1
+        game.players[0].hand.push eleven
+        game.players[0].hand.push three
+        game.players[0].hand.push another_three
+        game.players[1].hand.push Card.new 4, 2
+        game.players[1].hand.push Card.new 5, 2
+        game.players[1].hand.push Card.new 6, 2
+        game.players[2].hand.push Card.new 7, 2
+        game.players[2].hand.push Card.new 8, 2
+        game.players[2].hand.push Card.new 9, 2
+
+        game.first_move!
+
+        assert game.pile.include? three
+        assert game.pile.include? another_three
+    end
+
+    def test_play_from_hand_removes_from_hand
+        names = create_player_names
+        game = Game.new names, 3
+        eleven = Card.new 11, 4
+        two = Card.new 2, 3
+        another_two = Card.new 2, 1
+        game.players[0].hand.push eleven
+        game.players[0].hand.push two
+        game.players[0].hand.push another_two
+        to_lay = Array.new
+        to_lay.push two
+        to_lay.push another_two
+        game.play_from_hand! to_lay
+
+        assert (not(game.players[0].hand.include? two))
+        assert (not(game.players[0].hand.include? another_two))
+    end
+
+    def test_play_from_hand_adds_to_pile
+        names = create_player_names
+        game = Game.new names, 3
+        eleven = Card.new 11, 4
+        two = Card.new 2, 3
+        another_two = Card.new 2, 1
+        game.players[0].hand.push eleven
+        game.players[0].hand.push two
+        game.players[0].hand.push another_two
+        to_lay = Array.new
+        to_lay.push two
+        to_lay.push another_two
+        game.play_from_hand! to_lay
+
+        assert game.pile.include? two
+        assert game.pile.include? another_two
+    end
+
+    def test_play_from_hand_includes_card_in_last_move
+        names = create_player_names
+        game = Game.new names, 3
+        eleven = Card.new 11, 4
+        two = Card.new 2, 3
+        another_two = Card.new 2, 1
+        game.players[0].hand.push eleven
+        game.players[0].hand.push two
+        game.players[0].hand.push another_two
+        to_lay = Array.new
+        to_lay.push two
+        to_lay.push another_two
+        game.play_from_hand! to_lay
+
+        assert game.last_move =~ /TWO of CLUBS/i
+        assert game.last_move =~ /TWO of HEARTS/i
+    end
+       
 end
